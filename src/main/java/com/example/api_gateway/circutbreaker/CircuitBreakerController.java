71dad7e018806c9d22847cf41d9f2057a2ddd1b3
DashaVav <dashavavilova111002@gmail.com>
2024-08-05 10:53:18 +0300

    fix issues from sonar cloud

--- src/main/java/com/example/api_gateway/circutbreaker/CircuitBreakerController.java
+++ src/main/java/com/example/api_gateway/circutbreaker/CircuitBreakerController.java
@@ -3,15 +3,16 @@ package com.example.api_gateway.circutbreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CircuitBreakerController {
-    @RequestMapping("/fallback")
-    public ResponseEntity<ApiGatewayException> fallback() {
+    @RequestMapping(path = "/fallback", method = {RequestMethod.PUT, RequestMethod.POST})
+    public ResponseEntity<ApiGatewayError> fallback() {
         HttpStatus status = HttpStatus.SERVICE_UNAVAILABLE;
         return new ResponseEntity<>(
-                new ApiGatewayException("Service Unavailable", status.value()),
+                new ApiGatewayError("Service Unavailable", status.value()),
                 status);
     }
 }
